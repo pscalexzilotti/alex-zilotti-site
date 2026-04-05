ce16be3180e2881fb59b1a5b71e6f1b23becc80d
pscalexzilotti <pscalexzilotti@gmail.com>
2026-04-05 20:58:34 -0300

    Delete next.config.mjs

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
-  images: {
-    formats: ["image/avif", "image/webp"],
-  },
-};
-
-export default nextConfig;
